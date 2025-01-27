f115b237402f6bcc9bbefbbc48a800ce0ddcf424
GayatriiD <besmittenbyme@gmail.com>
2025-01-27 23:46:54 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,16 @@
+#Print Hello World
+print("Hello, World!")
+
+#Calculate the sum of 2 numbers
+def sum(a, b):
+    return a + b
+result1 = sum(5, 3)
+print(result1)
+
+#Find the square of a number
+def square(n):
+    return n ** 2
+result2 = square(4)
+print(result2)
+
+
